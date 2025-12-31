ad9d6e53241fe2930aae87a1a4c1cc473e88e1f7
l00pss <mammadli.vugar@hotmail.com>
2026-01-01 00:39:41 +0400

    Implement BitArray and SiftBloom structures with methods for setting and getting bits

--- bloom.go
+++ bloom.go
@@ -1,1 +1,50 @@
 package siftbloom
+
+import (
+	"errors"
+
+	"github.com/l00pss/helpme/result"
+)
+
+var (
+	IvalidArraySize = errors.New("Invalid array size")
+)
+
+type BitArray struct {
+	data []byte
+	size int
+}
+
+func NewBitArray(size int) result.Result[*BitArray] {
+	if size <= 0 {
+		return result.Err[*BitArray](IvalidArraySize)
+	}
+	return result.Ok(
+		&BitArray{
+			data: make([]byte, (size+7)/8),
+			size: size,
+		},
+	)
+}
+
+func (b *BitArray) Set(pos int, value bool) {
+	byteIndex := pos / 8
+	bitIndex := uint(pos % 8)
+
+	if value {
+		b.data[byteIndex] |= (1 << bitIndex)
+	} else {
+		b.data[byteIndex] &^= (1 << bitIndex)
+	}
+}
+
+func (b *BitArray) Get(pos int) bool {
+	byteIndex := pos / 8
+	bitIndex := uint(pos % 8)
+	return (b.data[byteIndex] & (1 << bitIndex)) != 0
+}
+
+type SiftBloom struct {
+	bits      BitArray
+	hashCount int
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/l00pss/siftbloom
 
 go 1.25
+
+require github.com/l00pss/helpme/result v1.0.1 // indirect
